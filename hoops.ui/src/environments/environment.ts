0fc27664ae467cbb8d95c38c35e3737104bcf19b
Richard Salit <richard.salit@gmail.com>
2022-01-01 15:29:05 -0500

    ironement file for test

--- hoops.ui/src/environments/environment.ts
+++ hoops.ui/src/environments/environment.ts
@@ -5,6 +5,6 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'https://localhost:5001',
+  apiUrl: 'https://csbchoopswebapi20210207173342.azurewebsites.net',
   environment: 'local'
 };
