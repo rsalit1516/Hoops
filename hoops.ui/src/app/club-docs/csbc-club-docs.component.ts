96bb3a170abb76db8fe5d6a2bd66b717b879059f
Richard Salit <richard.salit@gmail.com>
2022-01-01 19:54:51 -0500

    update to correct club docs files

--- hoops.ui/src/app/club-docs/csbc-club-docs.component.ts
+++ hoops.ui/src/app/club-docs/csbc-club-docs.component.ts
@@ -3,9 +3,11 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-csbc-club-docs',
   templateUrl: './csbc-club-docs.component.html',
-  styleUrls: ['./csbc-club-docs.component.css']
+  styleUrls: ['./csbc-club-docs.component.scss']
 })
 export class CsbcClubDocsComponent implements OnInit {
+
+  docDir = '../../assets/docs/';
 
   constructor() { }
 
