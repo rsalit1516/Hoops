155423b88313370f16ae1417b2f597d766fdcd88
Richard Salit <richard.salit@gmail.com>
2022-01-02 08:51:15 -0500

    updates to club docs links and UI

--- hoops.ui/src/app/club-docs/csbc-club-docs.component.ts
+++ hoops.ui/src/app/club-docs/csbc-club-docs.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-csbc-club-docs',
   templateUrl: './csbc-club-docs.component.html',
-  styleUrls: ['./csbc-club-docs.component.scss']
+  styleUrls: ['./club-docs.component.scss']
 })
 export class CsbcClubDocsComponent implements OnInit {
 
--- hoops.ui/src/app/club-docs/csbc-club-docs.module.ts
+++ hoops.ui/src/app/club-docs/csbc-club-docs.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
+import { CoreModule } from '../core/core.module';
 import { SharedModule } from '../shared/shared.module';
 import { CsbcClubDocsComponent } from './csbc-club-docs.component';
 
 @NgModule({
   imports: [
-    SharedModule
+    SharedModule,
+    CoreModule
   ],
   declarations: [
     CsbcClubDocsComponent
