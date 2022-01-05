ee32cc00a8da310b934aecc77e27cac5f219f9f6
Richard Salit <richard.salit@gmail.com>
2022-01-04 22:12:23 -0500

    update to back end environment

--- hoops.ui/src/environments/environment.prod.ts
+++ hoops.ui/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://csbchoops-api-prod.azurewebsites.net',
+  apiUrl: 'https://csbc-api-prod.azurewebsites.net',
   environment: 'production'
 };
