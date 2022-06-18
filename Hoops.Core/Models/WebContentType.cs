bf18b42990ee9f89afb1ede6d5cb195cbe6fcce7
Richard Salit <richard.salit@gmail.com>
2022-06-18 10:27:44 -0400

    Updates for angular 14

--- Hoops.Core/Models/WebContentType.cs
+++ Hoops.Core/Models/WebContentType.cs
@@ -1,8 +1,5 @@
-﻿using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using Microsoft.EntityFrameworkCore;
-
 
 namespace Hoops.Core.Models;
 
