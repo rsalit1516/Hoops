c36a9680e7777915dc7b341fb28911cd64ad1493
Richard Salit <richard.salit@gmail.com>
2024-10-16 14:38:50 -0400

    add-season html form

--- hoops.ui/src/app/admin/components/season-add/season-add.component.ts
+++ hoops.ui/src/app/admin/components/season-add/season-add.component.ts
@@ -1,10 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule, UntypedFormBuilder, Validators } from '@angular/forms';
+import { ReactiveFormsModule, UntypedFormBuilder, Validators } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
-import { MatFormField, MatFormFieldModule } from '@angular/material/form-field';
+import { MatDatepickerControl, MatDatepickerModule, MatDatepickerPanel } from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-import { RouterModule } from '@angular/router';
+import { MatSelectModule } from '@angular/material/select';
 import { SeasonService } from '@app/services/season.service';
 
 @Component({
@@ -17,7 +19,10 @@ import { SeasonService } from '@app/services/season.service';
     MatFormFieldModule,
     MatInputModule,
     MatCardModule,
-    MatFormFieldModule
+    MatFormFieldModule,
+    MatDatepickerModule,
+    MatButtonModule,
+    MatSelectModule,
   ],
   providers: [SeasonService],
   templateUrl: './season-add.component.html',
@@ -29,20 +34,26 @@ import { SeasonService } from '@app/services/season.service';
 export class SeasonAddComponent {
   title = 'Season';
   seasonService  = inject(SeasonService);
-  form = new FormGroup({
-    name: new FormControl('', { validators: [Validators.required] }), //this.division.divisionDescription,
-    seasonId: new FormControl('', {}),
-    divisionId: new FormControl('', {}), //this.division.seasonId,
-  });
-
+  form: any;
+startDatePicker!: MatDatepickerPanel<MatDatepickerControl<any>, any, any> ;
   constructor (private fb: UntypedFormBuilder) {
 
-    // this.form = this.fb.group({
-    //   name: ['', Validators.required], //this.division.divisionDescription,
-    //         seasonId: [''], //this.division.seasonId,
-    //   divisionId: [''], //this.division.seasonId,
-    // });
-  }
-
-  save() {}
+    this.form = this.fb.group({
+      name: ['', Validators.required], //this.division.divisionDescription,
+      seasonId: [''], //this.division.seasonId,
+      divisionId: [ '' ], //this.division.seasonId,
+      startDate: [ '' ],
+      endDate: [ '' ],
+      playerFee: [ '' ],
+      sponsorFee: [ '' ],
+      sponsorDiscount: [ '' ],
+      onlineStart: [ '' ],
+      onlineEnd: [ '' ],
+      gameSchedules: [ '' ],
+      currentSeason: [ '' ],
+      onLineRegistration: [ '' ],
+    }); }
+  save() { }
+  cancel() { }
+  new() {}
 }
