cced718906ca2b3d998476d5c402461f5e5f6389
rsalit1516 <richard.salit@gmail.com>
2024-11-17 09:35:15 -0500

    addedreference hoopscontext

--- src/Hoops.Api/Controllers/HouseholdController.cs
+++ src/Hoops.Api/Controllers/HouseholdController.cs
@@ -3,8 +3,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Hoops.Core.Models;
-using Hoops.Core;
 using Microsoft.EntityFrameworkCore;
+using Hoops.Infrastructure.Data;
 
 namespace Hoops.Controllers
 {
