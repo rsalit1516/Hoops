ef33d48eae51632d2254cbefecc6d09198c3f2f4
rsalit1516 <richard.salit@gmail.com>
2025-01-22 15:20:38 -0500

    Add admin household members component

---
+++ hoops.ui/src/app/admin/components/household-members/household-members.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-household-members',
+  imports: [],
+  templateUrl: './household-members.component.html',
+  styleUrl: './household-members.component.scss'
+})
+export class HouseholdMembersComponent {
+  pageTitle = 'Household members';
+/*
+Last Name
+First Name
+DOB
+Gender
+Register(button)
+*/
+}
