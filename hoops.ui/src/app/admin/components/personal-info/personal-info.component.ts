9d141806b779cd0c28f4a826783f4425923a3a7b
Richard Salit <richard.salit@gmail.com>
2025-02-01 09:20:42 -0500

    added personal info component

---
+++ hoops.ui/src/app/admin/components/personal-info/personal-info.component.ts
@@ -0,0 +1,39 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatRadioModule } from '@angular/material/radio';
+
+@Component({
+  selector: 'app-personal-info',
+  imports: [
+        CommonModule,
+        FormsModule,
+        ReactiveFormsModule,
+        ReactiveFormsModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatButtonModule,
+        MatIconModule,
+    MatCardModule,
+    MatDatepickerModule,
+    MatCheckboxModule,
+        MatRadioModule
+  ],
+  templateUrl: './personal-info.component.html',
+  styleUrls: [ './personal-info.component.scss',
+    '../../admin.component.scss',
+    '../../../shared/scss/forms.scss',
+    '../../../shared/scss/cards.scss',
+  ]
+
+})
+export class PersonalInfoComponent {
+
+}
