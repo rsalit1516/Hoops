efbc7a60704b7b59931b9a896b2a6b13cfa799a0
rsalit1516 <richard.salit@gmail.com>
2025-02-01 16:18:22 -0500

    updates to people search component

--- hoops.ui/src/app/admin/components/search-people/search-people.component.ts
+++ hoops.ui/src/app/admin/components/search-people/search-people.component.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { first } from 'rxjs-compat/operator/first';
 
 @Component({
   selector: 'csbc-search-people',
-  imports: [],
+  imports: [FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatCardModule
+  ],
   templateUrl: './search-people.component.html',
-  styleUrl: './search-people.component.scss'
+  styleUrls: ['./search-people.component.scss',
+    '../../admin.component.scss',
+    '../../../shared/scss/forms.scss',
+    '../../../shared/scss/cards.scss',
+  ],
 })
 export class SearchPeopleComponent {
+  pageTitle = 'Search People';
+  fb = inject(FormBuilder);
 
+  searchForm = this.fb.group({
+    lastName: [''],
+    firstName: [''],
+
+  }); onSearch () {
+    console.log('Searching...');
+  }
 }
