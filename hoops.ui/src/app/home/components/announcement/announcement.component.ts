94ba61e8862edf5596a12c96cf4ecc0e4210e3e1
Richard Salit <richard.salit@gmail.com>
2025-02-07 07:50:45 -0500

    updates to layout, tailwind

--- hoops.ui/src/app/home/components/announcement/announcement.component.ts
+++ hoops.ui/src/app/home/components/announcement/announcement.component.ts
@@ -4,21 +4,23 @@ import { MatCardModule } from '@angular/material/card';
 import { WebContent } from '@app/domain/webContent';
 
 @Component({
-    selector: 'csbc-announcement',
-    templateUrl: './announcement.component.html',
-    styleUrls: ['./../../../../Content/styles.scss',
-        '../../home.component.scss'],
-    imports: [CommonModule, MatCardModule]
+  selector: 'csbc-announcement',
+  templateUrl: './announcement.component.html',
+  styleUrls: [
+    '../../home.component.scss',
+    '../../../shared/scss/cards.scss',
+     ],
+  imports: [ CommonModule, MatCardModule ]
 })
 export class AnnouncementComponent implements OnInit {
   readonly info = input.required<WebContent>();
-  constructor() {}
+  constructor() { }
 
-  ngOnInit(): void {}
+  ngOnInit(): void { }
 
   hideLocationAndDateTime() {
 
     const info = this.info();
-    return ((info.location === '' || info.location === null) && (info.dateAndTime === '' || info.dateAndTime === null) );
+    return ((info.location === '' || info.location === null) && (info.dateAndTime === '' || info.dateAndTime === null));
   }
 }
