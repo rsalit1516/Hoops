cfd91be589837ddddbe18ec51052cd265822bcf9
Richard Salit <richard.salit@gmail.com>
2025-03-01 14:23:41 -0500

    updates to config

--- hoops.ui/src/+6985/updates to .ts
+++ hoops.ui/src/+6985/updates to .ts
@@ -4,27 +4,27 @@ import { enableProdMode, importProvidersFrom } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 
-import { environment } from './environments/environment';
-import { AppComponent } from './app/app.component';
+import { environment } from '../environments/environment';
+import { AppComponent } from '../app/app.component';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-import { reducers, metaReducers } from './app/reducers';
+import { reducers, metaReducers } from '../app/reducers';
 import { StoreModule } from '@ngrx/store';
-import { UserModule } from './app/user/user.module';
+import { UserModule } from '../app/user/user.module';
 import { LayoutModule } from '@angular/cdk/layout';
-import { LoginRoutingModule } from './app/login-routing.module';
-import { AdminModule } from './app/admin/admin.module';
-import { GamesModule } from './app/games/games.module';
-import { HomeModule } from './app/home/home.module';
+import { LoginRoutingModule } from '../app/login-routing.module';
+import { AdminModule } from '../app/admin/admin.module';
+import { GamesModule } from '../app/games/games.module';
+import { HomeModule } from '../app/home/home.module';
 import { withInterceptorsFromDi, provideHttpClient } from '@angular/common/http';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
-import { AppRoutingModule } from './app/app-routing.module';
+import { AppRoutingModule } from '../app/app-routing.module';
 import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
-import { DataService } from './app/services/data.service';
-import { TeamService } from './app/services/team.service';
-import { DivisionService } from './app/services/division.service';
-import { SeasonService } from './app/services/season.service';
+import { DataService } from '../app/services/data.service';
+import { TeamService } from '../app/services/team.service';
+import { DivisionService } from '../app/services/division.service';
+import { SeasonService } from '../app/services/season.service';
 
 if (environment.production) {
   enableProdMode();
