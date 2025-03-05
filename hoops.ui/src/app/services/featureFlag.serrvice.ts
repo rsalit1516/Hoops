cf6ddd648965197d9db394ed3bacf2ed206ebb8f
rsalit1516 <richard.salit@gmail.com>
2025-03-05 10:16:43 -0500

    changes to feature flag service

---
+++ hoops.ui/src/app/services/featureFlag.serrvice.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { FEATURE_FLAGS } from '../shared/constants';
+
+interface FeatureFlags {
+  adminHouseholds: boolean;
+  adminNotices: boolean;
+  adminPeople: boolean;
+  adminDirectors: boolean;
+  adminColors: boolean;
+  adminUsers: boolean;
+  adminGames: boolean;
+
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FeatureFlagService {
+  private featureFlags: FeatureFlags = FEATURE_FLAGS;
+
+  isFeatureEnabled (feature: keyof FeatureFlags): boolean {
+    return this.featureFlags[feature];
+  }
+}
