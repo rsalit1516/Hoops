25e5aceedbb0ca0176d2a2a23dbaa18502811f28
rsalit1516 <richard.salit@gmail.com>
2025-03-21 08:45:09 -0400

    removed some ngrx references and comment

--- hoops.ui/src/app/admin/admin.component.ts
+++ hoops.ui/src/app/admin/admin.component.ts
@@ -17,71 +17,56 @@ class MenuItem {
 }
 const SMALL_WIDTH_BREAKPOINT = 720;
 @Component({
-    templateUrl: './admin.component.html',
+  templateUrl: './admin.component.html',
   styleUrls: ['./admin.component.scss',
     '../shared/scss/forms.scss',
-    ],
-    standalone: true
+  ],
+  standalone: true
 })
 
 
-export class AdminComponent  implements OnInit {
+export class AdminComponent implements OnInit {
   readonly sidenav = viewChild(MatSidenav);
 
   opened = true;
   mode = 'side';
   menuItems!: MenuItem[];
   private mediaMatcher: MediaQueryList = matchMedia(
-    `(max-width: ${SMALL_WIDTH_BREAKPOINT}px)`
+    `(max-width: ${ SMALL_WIDTH_BREAKPOINT }px)`
   );
   options: UntypedFormGroup;
 
-  constructor(zone: NgZone, private router: Router, fb: UntypedFormBuilder,     private store: Store<fromContent.State>
-    ) {
+  constructor (zone: NgZone, private router: Router, fb: UntypedFormBuilder, private store: Store<fromContent.State>
+  ) {
     this.options = fb.group({
       bottom: 0,
       fixed: false,
       top: 0
     });
   }
-  ngOnInit() {
-    // this.store.dispatch(new contentActions.LoadAdminContent());
-   //  console.log('Called content');
-    // this.userService.isLoggedIn.subscribe(f => {
-    //   console.log(f);
-    //   this.isLoggedin = f;
-    //   this.router.navigate(['/login']);
-    // });
-
+  ngOnInit () {
     // this.user = this.userService.user;
     // TODO: get user types!
     this.menu();
-    // this.store.select(fromAdmin.getSelectedSeason).subscribe((season) => {
-    //   console.log(season);
-    //   if (season !== undefined) {
-    //     this.store.dispatch(new adminActions.LoadDivisions());
-    //     this.store.dispatch(new adminActions.LoadSeasonTeams());
+
+    // this.store.select(fromAdmin.getSelectedDivision).subscribe((division) => {
+    //   console.log(division);
+    //   if (division !== undefined) {
+    //     this.store.dispatch(new adminActions.LoadDivisionTeams());
     //   }
     // });
 
-    this.store.select(fromAdmin.getSelectedDivision).subscribe((division) => {
-      console.log(division);
-      if (division !== undefined) {
-        this.store.dispatch(new adminActions.LoadDivisionTeams());
-      }
-    });
-
-    this.store.select(fromAdmin.getSeasonDivisions).subscribe((divisions) => {
-      console.log(divisions);
-      this.store.dispatch(new adminActions.SetSelectedDivision(divisions[0]));
-      this.store.dispatch(new adminActions.LoadDivisionTeams);
-    });
-    this.store.select(fromAdmin.getSelectedTeam).subscribe((team) => {
-      console.log(team);
-      this.store.dispatch(new adminActions.LoadTeamGames);
-    });
+    // this.store.select(fromAdmin.getSeasonDivisions).subscribe((divisions) => {
+    //   console.log(divisions);
+    //   this.store.dispatch(new adminActions.SetSelectedDivision(divisions[0]));
+    //   this.store.dispatch(new adminActions.LoadDivisionTeams);
+    // });
+    // this.store.select(fromAdmin.getSelectedTeam).subscribe((team) => {
+    //   console.log(team);
+    //   this.store.dispatch(new adminActions.LoadTeamGames);
+    // });
   }
-    menu() {
+  menu () {
     this.menuItems = new Array<MenuItem>();
 
     this.menuItems.push({
