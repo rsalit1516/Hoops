faa40a35db13b3a4dc80111162bbd0349ababfb3
rsalit1516 <richard.salit@gmail.com>
2025-03-22 15:47:20 -0400

    add dashboard-games component

---
+++ hoops.ui/src/app/admin/components/dashboard-games/dashboard-games.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { DashboardGamesComponent } from './dashboard-games.component';
+
+describe('DashboardGamesComponent', () => {
+  let component: DashboardGamesComponent;
+  let fixture: ComponentFixture<DashboardGamesComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [DashboardGamesComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(DashboardGamesComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ hoops.ui/src/app/admin/components/dashboard-games/dashboard-games.component.ts
@@ -0,0 +1,23 @@
+import { Component, inject } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { AdminGamesListComponent } from '@app/admin/admin-games/admin-games-list/admin-games-list.component';
+import { GameService } from '@app/services/game.service';
+
+@Component({
+  selector: 'csbc-dashboard-games',
+  imports: [
+    MatCardModule,
+    AdminGamesListComponent
+  ],
+  templateUrl: './dashboard-games.component.html',
+  styleUrls: [
+    '../../../shared/scss/cards.scss',
+    '../../dashboard/admin-dashboard.component.scss',
+    '../../admin.component.scss'
+  ]
+})
+export class DashboardGamesComponent {
+  readonly #gameService = inject(GameService);
+  teamGames = this.#gameService.teamGames;
+  selectedTeam = this.#gameService.selectedTeam;
+}
