d911186072114b26e3a90022fd761ff7d1f82d87
Richard Salit <richard.salit@gmail.com>
2025-03-27 17:34:27 -0400

    update division select to use signal

--- hoops.ui/src/app/admin/admin-shared/division-select/division-select.component.ts
+++ hoops.ui/src/app/admin/admin-shared/division-select/division-select.component.ts
@@ -1,16 +1,15 @@
-import { Component, OnInit, Input, Output, EventEmitter, inject } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, inject } from '@angular/core';
 import { Division } from '@app/domain/division';
 import { Observable } from 'rxjs';
 import * as fromAdmin from '../../state';
 import { select, Store } from '@ngrx/store';
-import { FormControl, FormGroup, UntypedFormBuilder, UntypedFormControl, UntypedFormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { UntypedFormControl, UntypedFormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import * as adminActions from '../../state/admin.actions';
 import { MatOptionModule } from '@angular/material/core';
 import { MatSelectModule } from '@angular/material/select';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { AsyncPipe, NgFor } from '@angular/common';
 import { DivisionService } from '@app/services/division.service';
-// import { EventEmitter } from 'stream';
 
 @Component({
   selector: 'division-select',
@@ -31,33 +30,33 @@ import { DivisionService } from '@app/services/division.service';
 export class DivisionSelectComponent implements OnInit {
   // readonly selectedDivision = output<Division>();
   @Output() divisionChanged = new EventEmitter<Division>();
-  private divisionService = inject(DivisionService);
+  readonly divisionService = inject(DivisionService);
 
   selectForm!: UntypedFormGroup;
-  divisions$: Observable<Division[]>;
+  // divisions$: Observable<Division[]>;
   divisionComponent: UntypedFormControl | null | undefined;
   selectedDivision: Division | null = null;
 
   constructor (private store: Store<fromAdmin.State>) {
-    this.divisions$ = this.store.select(fromAdmin.getSeasonDivisions);
+    // this.divisions$ = this.store.select(fromAdmin.getSeasonDivisions);
   }
 
   ngOnInit (): void {
-    this.store.pipe(select(fromAdmin.getSelectedDivision)).subscribe((division) => {
-      if (division == null) {
-        this.selectedDivision = null;
-      } else {
-        if (division!.divisionId !== null) {
-          this.selectedDivision = division;
-          this.divisionService.updateSelectedDivision(division!);
-        }
-        this.divisionComponent?.setValue(division);
-      }
-    });
+    // this.store.pipe(select(fromAdmin.getSelectedDivision)).subscribe((division) => {
+    //   if (division == null) {
+    //     this.selectedDivision = null;
+    //   } else {
+    //     if (division!.divisionId !== null) {
+    //       this.selectedDivision = division;
+    //       this.divisionService.updateSelectedDivision(division!);
+    //     }
+    //     this.divisionComponent?.setValue(division);
+    //   }
+    // });
 
   }
   changeDivision (division: Division | null) {
-    this.store.dispatch(new adminActions.SetSelectedDivision(division));
+    // this.store.dispatch(new adminActions.SetSelectedDivision(division));
     this.divisionService.updateSelectedDivision(division!);
   }
   onChange (value: Division) {
