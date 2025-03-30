431c640c7de91516accf4e736f220d55fb888bba
Richard Salit <richard.salit@gmail.com>
2025-03-30 08:58:06 -0400

    putting this aside for now - not working.

--- hoops.ui/src/app/games/containers/playoffs-shell/playoffs-shell.component.spec.ts
+++ hoops.ui/src/app/games/containers/playoffs-shell/playoffs-shell.component.spec.ts
@@ -3,14 +3,12 @@ import { CommonModule } from '@angular/common';
 import { SchedulePlayoffsComponent } from '@app/games/components/schedule-playoffs/schedule-playoffs.component';
 import { Store } from '@ngrx/store';
 import * as fromGames from '../../state';
-import { GameService-deprecated } from '@app/games/game.service';
 import { PlayoffGame } from '@app/domain/playoffGame';
 import { PlayoffGameService } from '@app/services/playoff-game.service';
 
 @Component({
   selector: 'csbc-playoffs-shell',
   imports: [CommonModule, SchedulePlayoffsComponent],
-  providers: [GameService-deprecated, Store],
   template: `
     <div class="row">
     <h1>Playoffs</h1>
