d8e3aa1a28eccf74ed915773ab4b8f0017b3b1c3
rsalit1516 <richard.salit@gmail.com>
2025-05-14 14:24:57 -0400

    updates to clean up for testing.

---
+++ tests/Hoops.Infrastructure.Tests/TestDatabaseCollection.cs
@@ -0,0 +1,6 @@
+[CollectionDefinition("TestDatabaseCollection")]
+public class TestDatabaseCollection : ICollectionFixture<TestDatabaseFixture>
+{
+    // This class has no code and is never instantiated.
+    // Its purpose is simply to associate the TestDatabaseFixture with the test collection.
+}
