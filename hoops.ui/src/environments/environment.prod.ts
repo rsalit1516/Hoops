dc333dc906adefbf6a53dd848f620630199572b3
rsalit1516 <richard.salit@gmail.com>
2025-06-20 13:55:17 -0400

    minor updates for config

--- hoops.ui/src/environments/environment.prod.ts
+++ hoops.ui/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://csbchoops-api-prod.azurewebsites.net',
+  apiUrl: 'hoops-api-dev-dnggf0ewe3c2b9ae.eastus-01.azurewebsites.net',
   environment: 'production',
   showGameAdmin: false
 };
