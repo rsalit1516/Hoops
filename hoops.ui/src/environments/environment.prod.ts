09ffe41b84cd3dfd22202961b92251024ba7919e
rsalit1516 <richard.salit@gmail.com>
2025-06-20 16:41:25 -0400

    made sure the url is fully qualified.

--- hoops.ui/src/environments/environment.prod.ts
+++ hoops.ui/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'hoops-api-dev-dnggf0ewe3c2b9ae.eastus-01.azurewebsites.net',
+  apiUrl: 'https://hoops-api-dev-dnggf0ewe3c2b9ae.eastus-01.azurewebsites.net',
   environment: 'production',
   showGameAdmin: false,
   securityEnabled: false
