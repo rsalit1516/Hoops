bd5f7e4fe02806eb7ae88e57877f48de4703e781
rsalit1516 <rrsalit@hotmail.com>
2025-08-13 10:08:12 -0400

    updates to password toggle color and minor change to test.

--- hoops.ui/src/app/shared/guards/admin-guard.guard.spec.ts
+++ hoops.ui/src/app/shared/guards/admin-guard.guard.spec.ts
@@ -1,11 +1,11 @@
 import { TestBed } from '@angular/core/testing';
 import { CanActivateFn } from '@angular/router';
 
-import { adminGuardGuard } from './admin-guard.guard';
+import { adminGuard } from './admin-guard.guard';
 
 describe('adminGuardGuard', () => {
-  const executeGuard: CanActivateFn = (...guardParameters) => 
-      TestBed.runInInjectionContext(() => adminGuardGuard(...guardParameters));
+  const executeGuard: CanActivateFn = (...guardParameters) =>
+    TestBed.runInInjectionContext(() => adminGuard(...guardParameters));
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
