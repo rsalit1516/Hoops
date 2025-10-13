441791b0b9b2977b5b87c641d552dded8817da8d
rsalit1516 <rrsalit@hotmail.com>
2025-10-13 07:32:31 -0400

    changes to remove baseurl from tsconfig

--- hoops.ui/src/app/shared/services/feature-flags.ts
+++ hoops.ui/src/app/shared/services/feature-flags.ts
@@ -2,22 +2,23 @@
 import { computed, Injectable, signal } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { BehaviorSubject } from 'rxjs';
-import { environment } from 'environments/environment';
+import { environment } from './../../../environments/environment';
 
 @Injectable({ providedIn: 'root' })
 export class FeatureFlagService {
   private flagsSignal = signal<Record<string, boolean>>({});
 
-  constructor (private http: HttpClient) {
+  constructor(private http: HttpClient) {
     this.loadFlags();
   }
 
-  loadFlags (): void {
-    this.http.get<Record<string, boolean>>(environment.featureFlagPath)
-      .subscribe(flags => this.flagsSignal.set(flags));
+  loadFlags(): void {
+    this.http
+      .get<Record<string, boolean>>(environment.featureFlagPath)
+      .subscribe((flags) => this.flagsSignal.set(flags));
   }
 
-  isEnabled (flag: string): boolean {
+  isEnabled(flag: string): boolean {
     return this.flagsSignal()[flag] ?? false;
   }
 
