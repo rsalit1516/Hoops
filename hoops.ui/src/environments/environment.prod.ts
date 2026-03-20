8ac8a920a5cd733ed00309ec1c125a90fe67700e
rsalit1516 <rrsalit@hotmail.com>
2026-03-20 16:25:19 -0400

    fix for production function app access

--- hoops.ui/src/environments/environment.prod.ts
+++ hoops.ui/src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   apiUrl: 'https://hoops-api-dev-dnggf0ewe3c2b9ae.eastus-01.azurewebsites.net',
   functionsUrl:
-    'https://hoops-functions-dev-djeqhegwahbua7eq.eastus-01.azurewebsites.net',
+    'https://hoops-functions-prod-cjh4eggzfqgma8dc.eastus-01.azurewebsites.net',
   environment: 'production',
   showGameAdmin: false,
   securityEnabled: false,
