da89b693a554df471df2ff01b61c71c8597c664b
rsalit1516 <rrsalit@hotmail.com>
2026-03-28 10:38:34 -0400

    first step of folder re-org changes

--- hoops.ui/src/rxjs-extensions.ts
+++ hoops.ui/src/rxjs-extensions.ts
@@ -2,4 +2,4 @@ import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/do';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/toPromise';
-import { Observable } from 'rxjs/Observable';
+import { Observable } from 'rxjs';
