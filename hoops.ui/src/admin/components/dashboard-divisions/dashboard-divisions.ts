7eb6b839965d35579873468a4655ffb3c0b155e4
rsalit1516 <rrsalit@hotmail.com>
2026-03-30 11:34:47 -0400

    uupdates to dashboard-division component to show players

--- hoops.ui/src/admin/components/dashboard-divisions/dashboard-divisions.ts
+++ hoops.ui/src/admin/components/dashboard-divisions/dashboard-divisions.ts
@@ -1,15 +1,16 @@
 import { Component, computed, inject } from '@angular/core';
-import { MatCard, MatCardModule } from '@angular/material/card';
-import { MatListModule } from '@angular/material/list';
-import { Router } from '@angular/router';
+import { httpResource } from '@angular/common/http';
+import { MatCardModule } from '@angular/material/card';
+import { MatTableModule } from '@angular/material/table';
 import { Division } from '@app/domain/division';
+import { DraftListPlayer } from '@app/domain/draft-list-player';
 import { DivisionService } from '@app/services/division.service';
+import { SeasonService } from '@app/services/season.service';
+import { Constants } from '@shared/constants';
 
 @Component({
   selector: 'csbc-dashboard-divisions',
-  imports: [MatCardModule,
-    MatListModule,
-  ],
+  imports: [MatCardModule, MatTableModule],
   templateUrl: "./dashboard-divisions.html",
   styleUrls: [
     '../../../shared/scss/cards.scss',
@@ -19,15 +20,33 @@ import { DivisionService } from '@app/services/division.service';
 })
 export class DashboardDivisions {
   readonly #divisionService = inject(DivisionService);
-  readonly router = inject(Router);
+  readonly #seasonService = inject(SeasonService);
   divisionCount = computed(() => (this.#divisionService.seasonDivisions?.length ?? 0));
   seasonDivisions = this.#divisionService.seasonDivisions;
   selectedDivision: Division | undefined;
+  displayedColumns = ['division', 'players'];
 
-  goToDivision (division: Division) {
-    // this.store.dispatch(new adminActions.SetSelectedDivision(division));
-  this.#divisionService.updateSelectedDivision(division);
+  seasonPlayers = httpResource<DraftListPlayer[]>(() => {
+    const seasonId = this.#seasonService.selectedSeason()?.seasonId;
+    if (!seasonId) return undefined;
+    return `${Constants.GET_SEASON_PLAYERS_URL}/${seasonId}`;
+  });
+
+  playerCountByDivision = computed(() =>
+    (this.seasonPlayers.value() ?? []).reduce((map, p) => {
+      map.set(p.division, (map.get(p.division) ?? 0) + 1);
+      return map;
+    }, new Map<string, number>())
+  );
+
+  totalPlayerCount = computed(() => this.seasonPlayers.value()?.length ?? 0);
+
+  getPlayerCount(division: Division): number {
+    return this.playerCountByDivision().get(division.divisionDescription) ?? 0;
+  }
+
+  goToDivision(division: Division) {
+    this.#divisionService.updateSelectedDivision(division);
     this.selectedDivision = division;
-//    this.router.navigate(['/admin/division']);
   }
 }
