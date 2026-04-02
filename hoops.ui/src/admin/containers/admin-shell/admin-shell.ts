c1215254b21f89f163f84650173cefa02b75e7ff
rsalit1516 <rrsalit@hotmail.com>
2026-04-02 10:48:51 -0400

    fix admin menu for phones

--- hoops.ui/src/admin/containers/admin-shell/admin-shell.ts
+++ hoops.ui/src/admin/containers/admin-shell/admin-shell.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit, inject, signal } from '@angular/core';
+import { Component, computed, HostListener, OnInit, inject, signal } from '@angular/core';
 import { Store } from '@ngrx/store';
-import { RouterOutlet } from '@angular/router';
+import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 import * as adminActions from '../../state/admin.actions';
 import * as contentActions from '../../state/admin.actions';
@@ -10,43 +11,55 @@ import { ColorService } from '@app/admin/admin-shared/services/color.service';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatListModule } from '@angular/material/list';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 import { LocationService } from '@app/services/location.service';
 import { AdminShellSidebar } from '@app/admin/components/admin-shell-sidebar/admin-shell-sidebar';
 import { SeasonService } from '@app/services/season.service';
 import { DivisionService } from '@app/services/division.service';
-import { Observable } from 'rxjs';
-import { Season } from '@app/domain/season';
-import { toSignal } from '@angular/core/rxjs-interop';
 import { LoggerService } from '@app/services/logger.service';
 
 @Component({
   selector: 'csbc-admin-shell',
-  template: ` <div class="w-full mx-2 px-4 admin-container">
-    <div class="flex flex-wrap -mx-2">
-      <div class="w-2/12 px-2 nav-wrapper">
-        <app-admin-shell-sidebar class="" />
-      </div>
-      <div class="w-10/12 px-2">
-        <router-outlet class="" />
-      </div>
-    </div>
-  </div>`,
+  template: `
+    <mat-sidenav-container class="admin-sidenav-container">
+      <mat-sidenav [mode]="isMobile() ? 'over' : 'side'" [opened]="sidenavOpened()" (openedChange)="isOpen.set($event)" class="admin-sidenav">
+        <app-admin-shell-sidebar />
+      </mat-sidenav>
+      <mat-sidenav-content class="admin-sidenav-content">
+        <div class="flex items-center md:hidden menu-toggle-bar">
+          <button mat-icon-button (click)="isOpen.set(!isOpen())" aria-label="Toggle navigation">
+            <mat-icon>menu</mat-icon>
+          </button>
+        </div>
+        <div class="px-4">
+          <router-outlet />
+        </div>
+      </mat-sidenav-content>
+    </mat-sidenav-container>
+  `,
   styleUrls: ['./admin-shell.scss'],
   imports: [
     AdminShellSidebar,
+    MatSidenavModule,
     MatListModule,
     MatDividerModule,
-    RouterOutlet
-],
+    MatIconModule,
+    MatButtonModule,
+    RouterOutlet,
+  ],
 })
 export class AdminShell implements OnInit {
+  isMobile = signal(window.innerWidth < 768);
+  isOpen = signal(false);
+  sidenavOpened = computed(() => !this.isMobile() || this.isOpen());
+
+  @HostListener('window:resize')
+  onResize() {
+    this.isMobile.set(window.innerWidth < 768);
+  }
+
   events: string[] = [];
-  // opened: boolean;
-  showDirectors = false;
-  showHouseholds = false;
-  showPeople = false;
-  showColors = false;
-  showUsers = false;
   shouldRun = true;
   private seasonService = inject(SeasonService);
   private colorService = inject(ColorService);
@@ -54,14 +67,9 @@ export class AdminShell implements OnInit {
   locationService = inject(LocationService);
   store = inject(Store<fromAdmin.State>);
   divisionService = inject(DivisionService);
-  // seasons$: Observable<Season[]>;
-  //selectedSeason = signal<Season | undefined>(undefined);
-  // seasons = toSignal(this.store.select(fromAdmin.getSeasons));
-  // selectedSeason = toSignal(this.store.select(fromAdmin.getSelectedSeason));
+  readonly #router = inject(Router);
 
-  constructor () {
-    //    this.store.dispatch(new adminActions.LoadSeasons());
-  }
+  constructor() {}
 
   ngOnInit() {
     this.logger.debug('AdminShell ngOnInit');
@@ -73,6 +81,14 @@ export class AdminShell implements OnInit {
     });
     this.colorService.fetchColors();
     this.locationService.fetchLocations();
-  }
 
+    // Close drawer after navigation on mobile
+    this.#router.events
+      .pipe(filter(e => e instanceof NavigationEnd))
+      .subscribe(() => {
+        if (this.isMobile()) {
+          this.isOpen.set(false);
+        }
+      });
+  }
 }
