21edf4fb6acfd0bf1bdf5b7b762b2b1725969e69
rsalit1516 <rrsalit@hotmail.com>
2026-04-09 18:44:36 -0400

    updates for audit fields to primary tables

--- src/Hoops.Infrastructure/Data/hoopsContext.cs
+++ src/Hoops.Infrastructure/Data/hoopsContext.cs
@@ -186,7 +186,11 @@ public partial class hoopsContext : DbContext
             entity.Property(e => e.DirectorId).HasColumnName("DirectorID");
             entity.Property(e => e.CoDirectorId).HasColumnName("CoDirectorID");
             entity.Property(e => e.CreatedDate).HasColumnType("smalldatetime").HasDefaultValue(DateTime.Now);
-            entity.Property(e => e.CreatedUser).HasMaxLength(50);
+            entity.Property(e => e.CreatedUser);
+            entity.Property(e => e.ModifiedDate).HasColumnType("datetime");
+            entity.Property(e => e.ModifiedUser);
+            // Preserve legacy column that exists in DB but is not surfaced in the entity.
+            entity.Property<int?>("CompanyID").HasColumnName("CompanyID");
             entity.Property(e => e.DivisionDescription)
                 .HasColumnName("Div_Desc")
                 .HasMaxLength(50);
@@ -229,6 +233,18 @@ public partial class hoopsContext : DbContext
                 .HasForeignKey(d => d.CoDirectorId)
                 .OnDelete(DeleteBehavior.ClientSetNull)
                 .HasConstraintName("FK_Divisions_CoDirectors");
+
+            entity.HasOne<User>()
+                .WithMany()
+                .HasForeignKey(d => d.CreatedUser)
+                .OnDelete(DeleteBehavior.NoAction)
+                .HasConstraintName("FK_Divisions_Users_CreatedUser");
+
+            entity.HasOne<User>()
+                .WithMany()
+                .HasForeignKey(d => d.ModifiedUser)
+                .OnDelete(DeleteBehavior.NoAction)
+                .HasConstraintName("FK_Divisions_Users_ModifiedUser");
         });
 
         // modelBuilder.Entity<Dtproperties>(entity =>
@@ -365,8 +381,14 @@ public partial class hoopsContext : DbContext
             entity.Property(e => e.CoachId).HasColumnName("CoachID");
 
             entity.Property(e => e.CreatedDate).HasColumnType("smalldatetime").HasDefaultValue(DateTime.Now);
+            entity.Property(e => e.CreatedUser);
 
-            entity.Property(e => e.CreatedUser).HasMaxLength(20);
+            entity.Property(e => e.ModifiedDate).HasColumnType("datetime");
+
+            entity.Property(e => e.ModifiedUser);
+
+            // Preserve legacy column that exists in DB but is not surfaced in the entity.
+            entity.Property<int?>("CompanyID").HasColumnName("CompanyID");
 
             entity.Property(e => e.DivisionId).HasColumnName("DivisionID");
 
@@ -395,6 +417,18 @@ public partial class hoopsContext : DbContext
             entity.Property(e => e.SponsorId).HasColumnName("SponsorID");
 
             entity.Property(e => e.TeamId).HasColumnName("TeamID");
+
+            entity.HasOne<User>()
+                .WithMany()
+                .HasForeignKey(d => d.CreatedUser)
+                .OnDelete(DeleteBehavior.NoAction)
+                .HasConstraintName("FK_Players_Users_CreatedUser");
+
+            entity.HasOne<User>()
+                .WithMany()
+                .HasForeignKey(d => d.ModifiedUser)
+                .OnDelete(DeleteBehavior.NoAction)
+                .HasConstraintName("FK_Players_Users_ModifiedUser");
         });
 
         modelBuilder.Entity<Role>(entity =>
@@ -543,6 +577,8 @@ public partial class hoopsContext : DbContext
             entity.Property(e => e.GameTime).HasMaxLength(20);
             entity.Property(e => e.SeasonId).HasColumnName("SeasonID");
             entity.Property(e => e.DivisionId).HasColumnName("DivisionID");
+            entity.Property(e => e.CreatedDate).HasColumnType("datetime");
+            entity.Property(e => e.ModifiedDate).HasColumnType("datetime");
 
             // Configure the visiting team relationship
             entity.HasOne(d => d.VisitingTeam)
@@ -576,6 +612,18 @@ public partial class hoopsContext : DbContext
                 .HasForeignKey(d => d.LocationNumber)
                 .OnDelete(DeleteBehavior.ClientSetNull)
                 .HasConstraintName("FK_ScheduleGames_Location");
+
+            entity.HasOne<User>()
+                .WithMany()
+                .HasForeignKey(d => d.CreatedUser)
+                .OnDelete(DeleteBehavior.NoAction)
+                .HasConstraintName("FK_ScheduleGames_Users_CreatedUser");
+
+            entity.HasOne<User>()
+                .WithMany()
+                .HasForeignKey(d => d.ModifiedUser)
+                .OnDelete(DeleteBehavior.NoAction)
+                .HasConstraintName("FK_ScheduleGames_Users_ModifiedUser");
         });
 
         // modelBuilder.Entity<ScheduleGamesStats>(entity =>
@@ -605,11 +653,25 @@ public partial class hoopsContext : DbContext
             entity.Property(e => e.SchedulePlayoffId).HasColumnName("SchedulePlayoffID");
             entity.Property(e => e.DivisionId).HasColumnName("DivisionId");
             entity.Property(e => e.GameDate).HasColumnType("datetime2");
+            entity.Property(e => e.CreatedDate).HasColumnType("datetime");
+            entity.Property(e => e.ModifiedDate).HasColumnType("datetime");
 
             // Add compound unique constraint on ScheduleNumber + GameNumber
             entity.HasIndex(e => new { e.ScheduleNumber, e.GameNumber })
                   .IsUnique()
                   .HasDatabaseName("UQ_SchedulePlayoffs_Schedule_Game");
+
+            entity.HasOne<User>()
+                .WithMany()
+                .HasForeignKey(d => d.CreatedUser)
+                .OnDelete(DeleteBehavior.NoAction)
+                .HasConstraintName("FK_SchedulePlayoffs_Users_CreatedUser");
+
+            entity.HasOne<User>()
+                .WithMany()
+                .HasForeignKey(d => d.ModifiedUser)
+                .OnDelete(DeleteBehavior.NoAction)
+                .HasConstraintName("FK_SchedulePlayoffs_Users_ModifiedUser");
         });
 
 
@@ -623,7 +685,9 @@ public partial class hoopsContext : DbContext
             entity.Property(e => e.CompanyId).HasColumnName("CompanyID");
             entity.Property(e => e.ConvenienceFee).HasColumnType("money");
             entity.Property(e => e.CreatedDate).HasColumnType("smalldatetime").HasDefaultValue(DateTime.Now);
-            entity.Property(e => e.CreatedUser).HasMaxLength(50);
+            entity.Property(e => e.CreatedUser);
+            entity.Property(e => e.ModifiedDate).HasColumnType("datetime");
+            entity.Property(e => e.ModifiedUser);
             entity.Property(e => e.FromDate).HasColumnType("smalldatetime");
             entity.Property(e => e.ParticipationFee).HasColumnType("money");
             entity.Property(e => e.Description)
@@ -649,6 +713,18 @@ public partial class hoopsContext : DbContext
 
             entity.HasIndex(e => new { e.SignUpsDate, e.SignUpsEnd })
                 .HasDatabaseName("IX_Seasons_SignUpDates");
+
+            entity.HasOne<User>()
+                .WithMany()
+                .HasForeignKey(d => d.CreatedUser)
+                .OnDelete(DeleteBehavior.NoAction)
+                .HasConstraintName("FK_Seasons_Users_CreatedUser");
+
+            entity.HasOne<User>()
+                .WithMany()
+                .HasForeignKey(d => d.ModifiedUser)
+                .OnDelete(DeleteBehavior.NoAction)
+                .HasConstraintName("FK_Seasons_Users_ModifiedUser");
         });
 
         modelBuilder.Entity<SponsorFee>(entity =>
@@ -898,7 +974,14 @@ public partial class hoopsContext : DbContext
 
             entity.Property(e => e.CreatedDate).HasColumnType("smalldatetime").HasDefaultValue(DateTime.Now);
 
-            entity.Property(e => e.CreatedUser).HasMaxLength(50);
+            entity.Property(e => e.CreatedUser);
+
+            entity.Property(e => e.ModifiedDate).HasColumnType("datetime");
+
+            entity.Property(e => e.ModifiedUser);
+
+            // Preserve legacy SeasonID column + FK that exist in DB but are not surfaced in the entity.
+            entity.Property<int?>("SeasonID").HasColumnName("SeasonID");
 
             entity.Property(e => e.DivisionId).HasColumnName("DivisionID");
 
@@ -942,12 +1025,31 @@ public partial class hoopsContext : DbContext
                 .HasForeignKey(d => d.TeamColorId)
                 .OnDelete(DeleteBehavior.ClientSetNull)
                 .HasConstraintName("FK_Teams_Colors");
+
+            entity.HasOne<Season>()
+                .WithMany()
+                .HasForeignKey("SeasonID")
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_Teams_Seasons");
+
+            entity.HasOne<User>()
+                .WithMany()
+                .HasForeignKey(d => d.CreatedUser)
+                .OnDelete(DeleteBehavior.NoAction)
+                .HasConstraintName("FK_Teams_Users_CreatedUser");
+
+            entity.HasOne<User>()
+                .WithMany()
+                .HasForeignKey(d => d.ModifiedUser)
+                .OnDelete(DeleteBehavior.NoAction)
+                .HasConstraintName("FK_Teams_Users_ModifiedUser");
         });
 
         modelBuilder.Entity<WebContent>(entity =>
  {
      // entity.ToTable("WebContent");
      // entity.HasKey(entity => entity.WebContentId);
+     entity.Property(e => e.CreatedDate).HasColumnType("datetime");
      entity.Property(e => e.DateAndTime).HasMaxLength(50);
      entity.Property(e => e.ExpirationDate).HasColumnType("datetime");
      entity.Property(e => e.Location).HasMaxLength(50);
@@ -957,6 +1059,17 @@ public partial class hoopsContext : DbContext
      entity.Property(e => e.SubTitle).HasMaxLength(50);
      entity.Property(e => e.Title).HasMaxLength(50);
      entity.Property(e => e.Type).HasMaxLength(50);
+     entity.HasOne<User>()
+         .WithMany()
+         .HasForeignKey(d => d.CreatedUser)
+         .OnDelete(DeleteBehavior.NoAction)
+         .HasConstraintName("FK_WebContent_Users_CreatedUser");
+
+     entity.HasOne<User>()
+         .WithMany()
+         .HasForeignKey(d => d.ModifiedUser)
+         .OnDelete(DeleteBehavior.NoAction)
+         .HasConstraintName("FK_WebContent_Users_ModifiedUser");
      // entity.Navigation(w => w.WebContentType)
      // .UsePropertyAccessMode(PropertyAccessMode..Property);
  });
---
+++ src/Hoops.Infrastructure/Migrations/20260409223259_AddAuditFields.cs
@@ -0,0 +1,427 @@
+using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Hoops.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAuditFields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "CreatedUser",
+                table: "Seasons",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50,
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ModifiedDate",
+                table: "Seasons",
+                type: "datetime",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "ModifiedUser",
+                table: "Seasons",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CreatedUser",
+                table: "Divisions",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50,
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ModifiedDate",
+                table: "Divisions",
+                type: "datetime",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "ModifiedUser",
+                table: "Divisions",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CreatedUser",
+                table: "Teams",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50,
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ModifiedDate",
+                table: "Teams",
+                type: "datetime",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "ModifiedUser",
+                table: "Teams",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CreatedUser",
+                table: "Players",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(20)",
+                oldMaxLength: 20,
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ModifiedDate",
+                table: "Players",
+                type: "datetime",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "ModifiedUser",
+                table: "Players",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedDate",
+                table: "ScheduleGames",
+                type: "datetime",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "CreatedUser",
+                table: "ScheduleGames",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ModifiedDate",
+                table: "ScheduleGames",
+                type: "datetime",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "ModifiedUser",
+                table: "ScheduleGames",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedDate",
+                table: "SchedulePlayoffs",
+                type: "datetime",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "CreatedUser",
+                table: "SchedulePlayoffs",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ModifiedDate",
+                table: "SchedulePlayoffs",
+                type: "datetime",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "ModifiedUser",
+                table: "SchedulePlayoffs",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedDate",
+                table: "WebContent",
+                type: "datetime",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "CreatedUser",
+                table: "WebContent",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Seasons_CreatedUser",
+                table: "Seasons",
+                column: "CreatedUser");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Seasons_ModifiedUser",
+                table: "Seasons",
+                column: "ModifiedUser");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Divisions_CreatedUser",
+                table: "Divisions",
+                column: "CreatedUser");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Divisions_ModifiedUser",
+                table: "Divisions",
+                column: "ModifiedUser");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Teams_CreatedUser",
+                table: "Teams",
+                column: "CreatedUser");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Teams_ModifiedUser",
+                table: "Teams",
+                column: "ModifiedUser");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Players_CreatedUser",
+                table: "Players",
+                column: "CreatedUser");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Players_ModifiedUser",
+                table: "Players",
+                column: "ModifiedUser");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ScheduleGames_CreatedUser",
+                table: "ScheduleGames",
+                column: "CreatedUser");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ScheduleGames_ModifiedUser",
+                table: "ScheduleGames",
+                column: "ModifiedUser");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SchedulePlayoffs_CreatedUser",
+                table: "SchedulePlayoffs",
+                column: "CreatedUser");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SchedulePlayoffs_ModifiedUser",
+                table: "SchedulePlayoffs",
+                column: "ModifiedUser");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_WebContent_CreatedUser",
+                table: "WebContent",
+                column: "CreatedUser");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_WebContent_ModifiedUser",
+                table: "WebContent",
+                column: "ModifiedUser");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Seasons_Users_CreatedUser",
+                table: "Seasons",
+                column: "CreatedUser",
+                principalTable: "Users",
+                principalColumn: "UserID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Seasons_Users_ModifiedUser",
+                table: "Seasons",
+                column: "ModifiedUser",
+                principalTable: "Users",
+                principalColumn: "UserID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Divisions_Users_CreatedUser",
+                table: "Divisions",
+                column: "CreatedUser",
+                principalTable: "Users",
+                principalColumn: "UserID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Divisions_Users_ModifiedUser",
+                table: "Divisions",
+                column: "ModifiedUser",
+                principalTable: "Users",
+                principalColumn: "UserID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Teams_Users_CreatedUser",
+                table: "Teams",
+                column: "CreatedUser",
+                principalTable: "Users",
+                principalColumn: "UserID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Teams_Users_ModifiedUser",
+                table: "Teams",
+                column: "ModifiedUser",
+                principalTable: "Users",
+                principalColumn: "UserID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Players_Users_CreatedUser",
+                table: "Players",
+                column: "CreatedUser",
+                principalTable: "Users",
+                principalColumn: "UserID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Players_Users_ModifiedUser",
+                table: "Players",
+                column: "ModifiedUser",
+                principalTable: "Users",
+                principalColumn: "UserID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ScheduleGames_Users_CreatedUser",
+                table: "ScheduleGames",
+                column: "CreatedUser",
+                principalTable: "Users",
+                principalColumn: "UserID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ScheduleGames_Users_ModifiedUser",
+                table: "ScheduleGames",
+                column: "ModifiedUser",
+                principalTable: "Users",
+                principalColumn: "UserID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_SchedulePlayoffs_Users_CreatedUser",
+                table: "SchedulePlayoffs",
+                column: "CreatedUser",
+                principalTable: "Users",
+                principalColumn: "UserID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_SchedulePlayoffs_Users_ModifiedUser",
+                table: "SchedulePlayoffs",
+                column: "ModifiedUser",
+                principalTable: "Users",
+                principalColumn: "UserID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_WebContent_Users_CreatedUser",
+                table: "WebContent",
+                column: "CreatedUser",
+                principalTable: "Users",
+                principalColumn: "UserID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_WebContent_Users_ModifiedUser",
+                table: "WebContent",
+                column: "ModifiedUser",
+                principalTable: "Users",
+                principalColumn: "UserID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(name: "FK_Seasons_Users_CreatedUser", table: "Seasons");
+            migrationBuilder.DropForeignKey(name: "FK_Seasons_Users_ModifiedUser", table: "Seasons");
+            migrationBuilder.DropForeignKey(name: "FK_Divisions_Users_CreatedUser", table: "Divisions");
+            migrationBuilder.DropForeignKey(name: "FK_Divisions_Users_ModifiedUser", table: "Divisions");
+            migrationBuilder.DropForeignKey(name: "FK_Teams_Users_CreatedUser", table: "Teams");
+            migrationBuilder.DropForeignKey(name: "FK_Teams_Users_ModifiedUser", table: "Teams");
+            migrationBuilder.DropForeignKey(name: "FK_Players_Users_CreatedUser", table: "Players");
+            migrationBuilder.DropForeignKey(name: "FK_Players_Users_ModifiedUser", table: "Players");
+            migrationBuilder.DropForeignKey(name: "FK_ScheduleGames_Users_CreatedUser", table: "ScheduleGames");
+            migrationBuilder.DropForeignKey(name: "FK_ScheduleGames_Users_ModifiedUser", table: "ScheduleGames");
+            migrationBuilder.DropForeignKey(name: "FK_SchedulePlayoffs_Users_CreatedUser", table: "SchedulePlayoffs");
+            migrationBuilder.DropForeignKey(name: "FK_SchedulePlayoffs_Users_ModifiedUser", table: "SchedulePlayoffs");
+            migrationBuilder.DropForeignKey(name: "FK_WebContent_Users_CreatedUser", table: "WebContent");
+            migrationBuilder.DropForeignKey(name: "FK_WebContent_Users_ModifiedUser", table: "WebContent");
+
+            migrationBuilder.DropIndex(name: "IX_Seasons_CreatedUser", table: "Seasons");
+            migrationBuilder.DropIndex(name: "IX_Seasons_ModifiedUser", table: "Seasons");
+            migrationBuilder.DropIndex(name: "IX_Divisions_CreatedUser", table: "Divisions");
+            migrationBuilder.DropIndex(name: "IX_Divisions_ModifiedUser", table: "Divisions");
+            migrationBuilder.DropIndex(name: "IX_Teams_CreatedUser", table: "Teams");
+            migrationBuilder.DropIndex(name: "IX_Teams_ModifiedUser", table: "Teams");
+            migrationBuilder.DropIndex(name: "IX_Players_CreatedUser", table: "Players");
+            migrationBuilder.DropIndex(name: "IX_Players_ModifiedUser", table: "Players");
+            migrationBuilder.DropIndex(name: "IX_ScheduleGames_CreatedUser", table: "ScheduleGames");
+            migrationBuilder.DropIndex(name: "IX_ScheduleGames_ModifiedUser", table: "ScheduleGames");
+            migrationBuilder.DropIndex(name: "IX_SchedulePlayoffs_CreatedUser", table: "SchedulePlayoffs");
+            migrationBuilder.DropIndex(name: "IX_SchedulePlayoffs_ModifiedUser", table: "SchedulePlayoffs");
+            migrationBuilder.DropIndex(name: "IX_WebContent_CreatedUser", table: "WebContent");
+            migrationBuilder.DropIndex(name: "IX_WebContent_ModifiedUser", table: "WebContent");
+
+            migrationBuilder.DropColumn(name: "ModifiedDate", table: "Seasons");
+            migrationBuilder.DropColumn(name: "ModifiedUser", table: "Seasons");
+            migrationBuilder.DropColumn(name: "ModifiedDate", table: "Divisions");
+            migrationBuilder.DropColumn(name: "ModifiedUser", table: "Divisions");
+            migrationBuilder.DropColumn(name: "ModifiedDate", table: "Teams");
+            migrationBuilder.DropColumn(name: "ModifiedUser", table: "Teams");
+            migrationBuilder.DropColumn(name: "ModifiedDate", table: "Players");
+            migrationBuilder.DropColumn(name: "ModifiedUser", table: "Players");
+
+            migrationBuilder.DropColumn(name: "CreatedDate", table: "ScheduleGames");
+            migrationBuilder.DropColumn(name: "CreatedUser", table: "ScheduleGames");
+            migrationBuilder.DropColumn(name: "ModifiedDate", table: "ScheduleGames");
+            migrationBuilder.DropColumn(name: "ModifiedUser", table: "ScheduleGames");
+
+            migrationBuilder.DropColumn(name: "CreatedDate", table: "SchedulePlayoffs");
+            migrationBuilder.DropColumn(name: "CreatedUser", table: "SchedulePlayoffs");
+            migrationBuilder.DropColumn(name: "ModifiedDate", table: "SchedulePlayoffs");
+            migrationBuilder.DropColumn(name: "ModifiedUser", table: "SchedulePlayoffs");
+
+            migrationBuilder.DropColumn(name: "CreatedDate", table: "WebContent");
+            migrationBuilder.DropColumn(name: "CreatedUser", table: "WebContent");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CreatedUser",
+                table: "Seasons",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CreatedUser",
+                table: "Divisions",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CreatedUser",
+                table: "Teams",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CreatedUser",
+                table: "Players",
+                type: "nvarchar(20)",
+                maxLength: 20,
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+        }
+    }
+}
